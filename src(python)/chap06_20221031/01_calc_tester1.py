a687efe65fb37b31ec33a997b4cd8e68bdaab04e
u8yes <108450018+u8yes@users.noreply.github.com>
2022-10-31 13:07:41 +0900

    38

---
+++ src(python)/chap06_20221031/01_calc_tester1.py
@@ -0,0 +1,8 @@
+import my_package.calculator
+
+print(my_package.calculator.plus(10,5))
+print(my_package.calculator.minus(10,5))
+print(my_package.calculator.multiply(10,5))
+print(my_package.calculator.divide(10,5))
+
+
---
+++ src(python)/chap06_20221031/my_package/calculator.py
@@ -0,0 +1,11 @@
+def plus(x1, x2):
+    return x1 + x2
+
+def minus(x1, x2):
+    return x1 - x2
+
+def multiply(x1, x2):
+    return x1 * x2
+
+def divide(x1, x2):
+    return x1 / x2
