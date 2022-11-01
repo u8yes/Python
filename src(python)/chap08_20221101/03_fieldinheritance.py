69673dc0a08ca3d2b5033a7e4fb9becafdaa1a7c
u8yes <108450018+u8yes@users.noreply.github.com>
2022-11-01 18:22:32 +0900

    Create 03_fieldinheritance.py

---
+++ src(python)/chap08_20221101/03_fieldinheritance.py
@@ -0,0 +1,15 @@
+# 데이터 속성(field) 상속
+
+class A:
+    def __init__(self):
+        print("A.__init()__() 생성자 호출")
+        self.message = "Heavenly"
+
+class B(A):
+    def __init__(self):
+        print("B.__init__() 생성자 호출")
+
+if __name__ == '__main__':
+    obj = B()
+
+    print(obj.message)
