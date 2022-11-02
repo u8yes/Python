4e86fef644649386281f9e2b722009cedc613872
u8yes <108450018+u8yes@users.noreply.github.com>
2022-11-02 14:49:40 +0900

    Update 06_overriding1.py

--- src(python)/chap08_20221101/06_overriding1.py
+++ src(python)/chap08_20221101/06_overriding1.py
@@ -14,3 +14,14 @@ class C(A):
 
 # ---- 여기까지는 다중상속이 아니다. -----
 
+if __name__ == '__main__':
+    a = A()
+    a.method()
+
+    b = B()
+    b.method()
+
+    c = C()
+    c.method()
+
+    print()
