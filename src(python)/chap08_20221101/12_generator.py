9e0f7bc198add716b2061804ee3a1b3924f64a52
u8yes <108450018+u8yes@users.noreply.github.com>
2022-11-02 17:55:09 +0900

    40

--- src(python)/chap08_20221101/12_generator.py
+++ src(python)/chap08_20221101/12_generator.py
@@ -1,5 +1,3 @@
-
-
 def generator():
     yield 0
     yield 1
@@ -11,4 +9,19 @@ print(iterator.__next__())
 print(iterator.__next__())
 print(iterator.__next__())
 print(iterator.__next__())
-#print(iterator.__next__()) # error
+# print(iterator.__next__()) # error
+
+################################################################
+
+def yourRange(start, end):
+    current = start
+
+    while current < end:
+        yield current
+        current += 1
+
+if __name__ == '__main__':
+    print("============================")
+    for i in yourRange(0, 4):
+        print(i)
+
